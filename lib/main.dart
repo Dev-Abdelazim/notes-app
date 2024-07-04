ea073393ea1d1a537b00ee2e9268a395609ff179
Dev-Abdelazim <dev.abdelazim@gmail.com>
2024-07-04 14:34:57 +0300

    change them and create views folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
 
 void main(List<String> args) {
   runApp(const NotesApp());
@@ -9,12 +10,9 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Notes App"),
-        ),
-      ),
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const NotesView(),
     );
   }
 }
