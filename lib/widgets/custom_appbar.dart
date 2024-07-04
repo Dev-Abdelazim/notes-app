73ba7076e9d5da6c36bce0175c641d7766f1b5c2
Dev-Abdelazim <dev.abdelazim@gmail.com>
2024-07-04 15:00:19 +0300

    create custom appbar,  add widgets folder and create custom custom notes body, custom search icon

---
+++ lib/widgets/custom_appbar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.only(top: 42),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Notes',
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+          CustomSearchIcon()
+        ],
+      ),
+    );
+  }
+}
